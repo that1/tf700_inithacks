d79106527684976843537d69fc142f879bb60023
that <github@that.at>
2014-02-28 20:14:30 +0100

    built with gcc-4.8

--- mk.sh
+++ mk.sh
@@ -1,4 +1,4 @@
-arm-linux-androideabi-gcc-4.7 --sysroot=/opt/android-ndk/platforms/android-14/arch-arm/ -static init.c -o init -Wall
+arm-linux-androideabi-gcc-4.8 --sysroot=/opt/android-ndk/platforms/android-14/arch-arm/ -static init.c -o init -Wall
 arm-linux-androideabi-strip init
 
 #gcc -static init.c -o init
