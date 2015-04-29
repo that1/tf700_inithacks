cbdfc5780e84dcfa00d5ae7417a2126d1be78564
that <github@that.at>
2015-04-29 22:36:55 +0200

    mk.sh: build as C99
    My C style is C++ without classes and templates.

--- mk.sh
+++ mk.sh
@@ -1,3 +1,3 @@
-arm-linux-androideabi-gcc-4.8 --sysroot=/opt/android-ndk/platforms/android-14/arch-arm/ -static -s -o init -Wall init.c
+arm-linux-androideabi-gcc-4.8 --sysroot=/opt/android-ndk/platforms/android-14/arch-arm/ -static -s -o init -std=c99 -pedantic -Wall init.c
 
 #gcc -static -o init -Wall init.c
